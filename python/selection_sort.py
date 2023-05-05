b213282e61bafd35f51ca9d84c26df4d0006ca97
Pedro Luiz <pedroluizlsg@gmail.com>
2023-05-05 16:18:35 -0300

    add selection_sort

--- python/selection_sort.py
+++ python/selection_sort.py
@@ -2,9 +2,14 @@ def selection_sort(array: list):
     for i in range(len(array)):
         minimum = i
 
+        print('1º if - posição: ', minimum, 'valor: ', array[minimum])
         for j in range(i + 1, len(array)):
             if (array[j] < array[minimum]):
+                print('PRÉ 2º if - posição: ',
+                      minimum, 'valor: ', array[minimum])
                 minimum = j
+                print('PÓS 2º if - posição: ',
+                      minimum, 'valor: ', array[minimum])
 
         array[minimum], array[i] = array[i], array[minimum]
 
